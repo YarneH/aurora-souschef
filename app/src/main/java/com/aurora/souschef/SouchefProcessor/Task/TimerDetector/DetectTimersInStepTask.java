7da70afdcb28749ce10aed1584b18df85321db2e
Piet Verheye <piet.verheye@ugent.be>
2019-02-25 17:47:08 +0100

    NLP detection of timers in recipe steps

--- app/src/main/java/com/aurora/souschef/SouchefProcessor/Task/TimerDetector/DetectTimersInStepTask.java
+++ app/src/main/java/com/aurora/souschef/SouchefProcessor/Task/TimerDetector/DetectTimersInStepTask.java
@@ -8,6 +8,28 @@ import com.aurora.souschef.SouchefProcessor.Recipe.RecipeStep;
 import com.aurora.souschef.SouchefProcessor.Recipe.RecipeTimer;
 import com.aurora.souschef.SouchefProcessor.Task.ProcessingTask;
 
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.AnnotationPipeline;
+import edu.stanford.nlp.pipeline.POSTaggerAnnotator;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.pipeline.TokenizerAnnotator;
+import edu.stanford.nlp.pipeline.WordsToSentencesAnnotator;
+import edu.stanford.nlp.time.TimeAnnotations;
+import edu.stanford.nlp.time.TimeAnnotator;
+import edu.stanford.nlp.time.TimeExpression;
+import edu.stanford.nlp.util.CoreMap;
+
 /**
  * A task that detects timers in mRecipeSteps
  */
@@ -42,20 +64,34 @@ public class DetectTimersInStepTask extends ProcessingTask {
      * @return A timer detected in the recipeStep
      */
     private List<RecipeTimer> detectTimer(RecipeStep recipeStep) {
-
-        //dummy
         List<RecipeTimer> list = new ArrayList<>();
-        try {
 
-            if (recipeStep.getDescription().contains("9 minutes")) {
-                list.add(new RecipeTimer(9 * 60));
-            } else {
-                list.add(new RecipeTimer(3 * 60, 3 * 60));
+        Properties props = new Properties();
+
+        AnnotationPipeline pipeline = new AnnotationPipeline();
+        pipeline.addAnnotator(new TokenizerAnnotator(false));
+        pipeline.addAnnotator(new WordsToSentencesAnnotator(false));
+        pipeline.addAnnotator(new POSTaggerAnnotator(false));
+        pipeline.addAnnotator(new TimeAnnotator("sutime", props));
+
+        Annotation recipeStepAnnotated = new Annotation(recipeStep.getDescription());
+
+        pipeline.annotate(recipeStepAnnotated);
+
+        List<CoreMap> timexAnnotations = recipeStepAnnotated.get(TimeAnnotations.TimexAnnotations.class);
+        for (CoreMap cm : timexAnnotations) {
+            List<CoreLabel> tokens = cm.get(CoreAnnotations.TokensAnnotation.class);
+            int recipeStepSeconds = (int) cm.get(TimeExpression.Annotation.class).getTemporal()
+                    .getDuration().getJodaTimeDuration().getStandardSeconds();
+
+            try{
+                list.add(new RecipeTimer(recipeStepSeconds));
             }
-
-        } catch (IllegalArgumentException tvie) {
-            //TODO do something meaningful
+            catch (IllegalArgumentException tvie) {
+                //TODO do something meaningful
+            }
         }
+
         return list;
     }
 }

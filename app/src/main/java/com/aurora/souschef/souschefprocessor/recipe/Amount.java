e56cd2044f89474c808caf15db87e1a869df1f79
Luca Renders <luca.renders@hotmail.com>
2019-02-26 15:11:35 +0100

    Rename packages
    change icon

---
+++ app/src/main/java/com/aurora/souschef/souschefprocessor/recipe/Amount.java
@@ -0,0 +1,43 @@
+package com.aurora.souschef.souschefprocessor.recipe;
+
+import java.util.Objects;
+
+public class Amount {
+
+    private double mValue;
+    private String mUnit;
+
+    public Amount(double mValue, String unit) {
+        this.mValue = mValue;
+        this.mUnit = unit;
+    }
+
+    public double getValue() {
+        return mValue;
+    }
+
+    public String getUnit() {
+        return mUnit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mUnit, mValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Amount) {
+            Amount a = (Amount) o;
+            if (a.getUnit().equals(mUnit) && a.getValue() == mValue) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return mValue + " " + mUnit;
+    }
+}

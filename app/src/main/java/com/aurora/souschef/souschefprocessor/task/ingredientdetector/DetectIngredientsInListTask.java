fda83972215bfb935137bb2f1e51614b76eebae8
Luca Renders <luca.renders@hotmail.com>
2019-03-06 18:27:13 +0100

    Documentation upgraded

--- app/src/main/java/com/aurora/souschef/souschefprocessor/task/ingredientdetector/DetectIngredientsInListTask.java
+++ app/src/main/java/com/aurora/souschef/souschefprocessor/task/ingredientdetector/DetectIngredientsInListTask.java
@@ -23,6 +23,8 @@ import static android.content.ContentValues.TAG;
 
 /**
  * Detects the mIngredients in the list of mIngredients
+ * It has a CRFClassifier that classifies a sentence containing an ingredient to UNIT, QUANTITY
+ * and NAME
  */
 public class DetectIngredientsInListTask extends AbstractProcessingTask {
 
@@ -31,11 +33,23 @@ public class DetectIngredientsInListTask extends AbstractProcessingTask {
     private static final String[] NUMBERS_TO_TWELVE = {"zero", "one", "two", "three", "four", "five",
             "six", "seven", "eight", "nine", "ten", "eleven", "twelve"};
     // multiples of ten are also spelled out
-    private static final String[] MULTIPLES_OF_TEN = {"zero", "ten", "twenty", "thirty", "fourty",
+    private static final String[] MULTIPLES_OF_TEN = {"zero", "ten", "twenty", "thirty", "forty",
             "fifty", "sixty", "seventy", "eighty", "ninety", "hundred"};
-    private static final int FRACTION_SIZE = 2;
+
+    // The size if a string representing a fraction is split on the regex "/"
+    private static final int FRACTION_LENGTH = 2;
+    // The size if a string representing a number (non-fraction) is split on the regex "/"
     private static final int NON_FRACTION_LENGTH = 1;
+
+    // The number 10
     private static final double TEN = 10;
+
+    // Strings representing the classes of the classifier
+    private static final String QUANTITY = "QUANTITY";
+    private static final String UNIT = "UNIT";
+    private static final String NAME = "NAME";
+
+    //The classifier to detect ingredients
     private CRFClassifier<CoreLabel> crf;
 
     public DetectIngredientsInListTask(RecipeInProgress recipeInProgress) {
@@ -43,6 +57,11 @@ public class DetectIngredientsInListTask extends AbstractProcessingTask {
 
     }
 
+    /**
+     * Checks if the string is a spelled out version of the numbers 0 to 12 or a multiple of 10 (up to 100)
+     * @param s The string to be checked
+     * @return The numeric representation of the string
+     */
     private static double calculateNonParsableQuantity(String s) {
         String lower = s.toLowerCase(Locale.ENGLISH);
         // check if  number is 0-12
@@ -59,7 +78,7 @@ public class DetectIngredientsInListTask extends AbstractProcessingTask {
             }
         }
 
-        // if not one of the previous cases consider wrongly labeled and return zero
+        // if not one of the previous cases consider wrongly labeled
         return 0.0;
     }
 
@@ -104,9 +123,17 @@ public class DetectIngredientsInListTask extends AbstractProcessingTask {
         return returnSet;
     }
 
+    /**
+     * Detects the ingredient described in the line and constructs an Ingredient object with this information
+     *
+     * @param line The line in which the ingredient is to be detected
+     * @return an Ingredient object constructed with the information from the line
+     */
     private Ingredient detectIngredient(String line) {
+        // Initialize
         Ingredient ing = null;
-        double quantity = 0.0;
+        // if no value present, default to 1.0 'one'
+        double quantity = 1.0;
         String unit = "";
         String name = "";
         try {
@@ -135,23 +162,23 @@ public class DetectIngredientsInListTask extends AbstractProcessingTask {
 
 
             //for now get the first element
-            if (map.get("UNIT") != null) {
-                unit = map.get("UNIT").get(0).toString();
+            if (map.get(UNIT) != null) {
+                unit = map.get(UNIT).get(0).toString();
             }
             // return everything labeled name
-            List<CoreLabel> nameList = map.get("NAME");
+            List<CoreLabel> nameList = map.get(NAME);
             if (nameList != null) {
                 name = buildName(nameList);
 
             }
-            if (map.get("QUANTITY") != null) {
-                quantity += calculateQuantity(map.get("QUANTITY"));
+            if (map.get(QUANTITY) != null) {
+                quantity += calculateQuantity(map.get(QUANTITY));
             }
             // if quantity is seen as negative revert
             if (quantity < 0.0) {
                 quantity = -quantity;
             }
-         
+
             ing = new Ingredient(name, unit, quantity);
             return ing;
         } catch (IOException | ClassNotFoundException exception) {
@@ -160,21 +187,43 @@ public class DetectIngredientsInListTask extends AbstractProcessingTask {
         return null;
     }
 
+    /**
+     * Builds the name of the ingredient using a list of tokens that were classified as NAME
+     *
+     * @param nameList the list of tokens
+     * @return a string which is a concatenation of all the words in the list
+     */
     private String buildName(List<CoreLabel> nameList) {
-        // return everey entity labeled name
+        // return every entity labeled name
         StringBuilder bld = new StringBuilder();
         for (CoreLabel cl : nameList) {
-            bld.append(cl.word() + " ");
+            if (NAME.equals(cl.get(CoreAnnotations.AnswerAnnotation.class))) {
+                bld.append(cl.word() + " ");
+            }
         }
         // delete last added space
         bld.deleteCharAt(bld.length() - 1);
         return bld.toString();
     }
 
+    /**
+     * Calculates the quantity based on list with tokens labeled quantity
+     *
+     * @param list The list on which to calculate the quantity
+     * @return a double representing the calculated value, if no value could be detected 1.0 is
+     * returned
+     */
     private double calculateQuantity(List<CoreLabel> list) {
         double result = 0.0;
-        // for now first element
-        CoreLabel element = list.get(0);
+        // for now first element labeled as quantity
+        boolean found = false;
+        CoreLabel element = null;
+        for (int i = 0; i < list.size() && !found; i++) {
+            element = list.get(i);
+            if (QUANTITY.equals(element.get(CoreAnnotations.AnswerAnnotation.class))) {
+                found = true;
+            }
+        }
         String representation = element.word();
 
         // split on all whitespace characters
@@ -183,7 +232,7 @@ public class DetectIngredientsInListTask extends AbstractProcessingTask {
 
             String[] fraction = s.split("/");
             try {
-                if (fraction.length == FRACTION_SIZE) {
+                if (fraction.length == FRACTION_LENGTH) {
 
                     double numerator = Double.parseDouble(fraction[0]);
                     double denominator = Double.parseDouble(fraction[1]);
@@ -198,8 +247,10 @@ public class DetectIngredientsInListTask extends AbstractProcessingTask {
                 result += calculateNonParsableQuantity(s);
             }
         }
-
-
+        if (result == 0.0) {
+            // if no quantity value was detected return 1.0 "one"
+            return 1.0;
+        }
         return result;
     }
 }

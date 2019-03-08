ff15cc98507fe25c09f358316e2550de2d098a96
Piet Verheye <piet.verheye@ugent.be>
2019-03-08 10:06:08 +0100

    Update DetectTimersInStepTask.java
    Hardcoded solutions for symbol notation and verbose fraction timers

--- app/src/main/java/com/aurora/souschef/souschefprocessor/task/timerdetector/DetectTimersInStepTask.java
+++ app/src/main/java/com/aurora/souschef/souschefprocessor/task/timerdetector/DetectTimersInStepTask.java
@@ -8,13 +8,16 @@ import com.aurora.souschef.souschefprocessor.task.AbstractProcessingTask;
 import com.aurora.souschef.souschefprocessor.task.RecipeInProgress;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
@@ -34,6 +37,14 @@ import edu.stanford.nlp.util.CoreMap;
  */
 public class DetectTimersInStepTask extends AbstractProcessingTask {
     int mStepIndex;
+
+    private static final String FRACTION_HALF = "half";
+    private static final String FRACTION_QUARTER = "quarter";
+
+    Map<String, Double> fractionMultiplier = new HashMap<String, Double>() {{
+        put(FRACTION_HALF, 0.5);
+        put(FRACTION_QUARTER, 0.25);
+    }};
 
     public DetectTimersInStepTask(RecipeInProgress recipeInProgress, int stepIndex) {
         super(recipeInProgress);
@@ -56,7 +67,6 @@ public class DetectTimersInStepTask extends AbstractProcessingTask {
         recipeStep.setRecipeTimers(recipeTimers);
     }
 
-
     /**
      * Detects the timer in a recipeStep
      *
@@ -78,15 +88,59 @@ public class DetectTimersInStepTask extends AbstractProcessingTask {
 
         pipeline.annotate(recipeStepAnnotated);
 
+        // Map fractions to their start position in the recipe step
+        // to compare their relative position to NER DURATION tags
+        HashMap<Integer, String> fractionPositions = new HashMap<>();
+
+        // Pattern for smybol notations
+        String pattern = "(\\d+)[h|m|s|H|M|S]";
+        Pattern symbolPattern = Pattern.compile(pattern);
+
+        List<CoreLabel> allTokens = recipeStepAnnotated.get(CoreAnnotations.TokensAnnotation.class);
+        for(CoreLabel token : allTokens){
+            if(token.originalText().equals(FRACTION_HALF) ){
+                fractionPositions.put(token.beginPosition(), FRACTION_HALF);
+            }
+            else if(token.originalText().equals(FRACTION_QUARTER)){
+                fractionPositions.put(token.beginPosition(), FRACTION_QUARTER);
+            }
+            if(token.originalText().matches(pattern)){
+                try {
+                    list.add(new RecipeTimer(getSecondsFromFormattedString("PT" + token.originalText())));
+                } catch (IllegalArgumentException iae) {
+                    //TODO do something meaningful
+                };
+            }
+        }
+
+
         List<CoreMap> timexAnnotations = recipeStepAnnotated.get(TimeAnnotations.TimexAnnotations.class);
         for (CoreMap cm : timexAnnotations) {
-            List<CoreLabel> tokens = cm.get(CoreAnnotations.TokensAnnotation.class);
             int recipeStepSeconds;
             SUTime.Temporal temporal = cm.get(TimeExpression.Annotation.class).getTemporal();
+
+            //only one value
             if (!(temporal.getDuration() instanceof SUTime.DurationRange)) {
-                //only one value
                 recipeStepSeconds = (int) temporal
                         .getDuration().getJodaTimeDuration().getStandardSeconds();
+
+                CoreLabel timexToken = cm.get(CoreAnnotations.TokensAnnotation.class).get(0);
+                if(!fractionPositions.isEmpty()){
+                    for (Map.Entry<Integer, String> fractionPosition : fractionPositions.entrySet()) {
+                        int relPosition = fractionPosition.getKey() - timexToken.beginPosition();
+                        // Fraction in front of timex tag is assumed to be a decreasing multiplier
+                        // e.g. "half an hour" = 0.5 * 3600s
+                        if(-15 < relPosition && relPosition < 0){
+                            recipeStepSeconds *= fractionMultiplier.get(fractionPosition.getValue());
+                        }
+                        // Fraction behind timex tag is assumed to be an increasing multiplier
+                        // e.g. "for an hour and a half" = 1.5 * 3600s
+                        else if(0 < relPosition && relPosition < 15){
+                            recipeStepSeconds *= (1+fractionMultiplier.get(fractionPosition.getValue()));
+                        }
+                    }
+                }
+
                 try {
                     list.add(new RecipeTimer(recipeStepSeconds));
                 } catch (IllegalArgumentException iae) {
@@ -108,7 +162,6 @@ public class DetectTimersInStepTask extends AbstractProcessingTask {
                     //TODO do something meaningful
                 }
             }
-
         }
 
         return list;
@@ -119,11 +172,11 @@ public class DetectTimersInStepTask extends AbstractProcessingTask {
         String number = string.substring(2, string.length() - 1);
         int num = Integer.parseInt(number);
         char unit = string.charAt(string.length() - 1);
-        if (unit == 'M') {
+        if (Character.toLowerCase(unit) == 'm') {
             return num * 60;
-        } else if (unit == 'H') {
+        } else if (Character.toLowerCase(unit) == 'h') {
             return num * 60 * 60;
-        } else if (unit == 'S') {
+        } else if (Character.toLowerCase(unit) == 's') {
             return num;
         }
         return 0;

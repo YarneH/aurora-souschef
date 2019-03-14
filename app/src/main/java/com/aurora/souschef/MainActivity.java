c38206ccae564bff7f039767ca892f064cb64a93
jotack <jonas.tack@ugent.be>
2019-03-14 15:34:08 +0100

    Add timer popups

--- app/src/main/java/com/aurora/souschef/MainActivity.java
+++ app/src/main/java/com/aurora/souschef/MainActivity.java
@@ -1,14 +1,34 @@
 package com.aurora.souschef;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.aurora.auroralib.Constants;
 
 public class MainActivity extends AppCompatActivity {
+    private final static int AMOUNT_SEC_IN_HOUR = 3600;
+    private final static int AMOUNT_SEC_IN_HALF_HOUR = 1800;
+    private final static int AMOUNT_SEC_IN_QUARTER = 900;
+    private final static int AMOUNT_SEC_IN_MIN = 60;
+    private final static int MINUTE_STEP = 60;
+    private final static int HALF_MINUTE_STEP = 30;
+    private final static int QUARTER_MINUTE_STEP = 15;
+    private final static int SECOND_STEP = 1;
+    private final static int PERCENT = 100;
+
+    // TODO: remove dummy values
+    private final static int DUMMY_LOWERBOUND = 45 * AMOUNT_SEC_IN_MIN;
+    private final static int DUMMY_UPPERBOUND = 90 * AMOUNT_SEC_IN_MIN;
+
+
     private TextView mTextView;
 
     public MainActivity() {
@@ -37,5 +57,86 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
+
+        // TODO Change location of timer
+        setTimerPopup();
+    }
+
+    // TODO Change location of timer
+    public void setTimerPopup() {
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptView = li.inflate(R.layout.card_timer, null);
+
+        int lowerBound = DUMMY_LOWERBOUND;
+        int upperBound = DUMMY_UPPERBOUND;
+        int difference = upperBound - lowerBound;
+
+        SeekBar seekBar = (SeekBar) promptView.findViewById(R.id.sk_timer);
+
+        int step;
+        if (difference >= AMOUNT_SEC_IN_HOUR) {
+            step = MINUTE_STEP;
+        } else if (difference >= AMOUNT_SEC_IN_HALF_HOUR) {
+            step = HALF_MINUTE_STEP;
+        } else if (difference > AMOUNT_SEC_IN_QUARTER) {
+            step = QUARTER_MINUTE_STEP;
+        } else {
+            step = SECOND_STEP;
+        }
+
+        final TextView seekBarValue = (TextView) promptView.findViewById(R.id.tv_timer);
+        seekBarValue.setText(convertTimeToString(lowerBound));
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // TODO Auto-generated method stub
+                double progressValue = ((double) (upperBound - lowerBound)) * progress / PERCENT;
+                int incrementValue = (int) Math.floor(progressValue / step) * step;
+                int newValue = lowerBound + incrementValue;
+
+                String timerText = convertTimeToString(newValue);
+
+                seekBarValue.setText(timerText);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Auto-generated method stub (not needed)
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Auto-generated method stub (not needed)
+            }
+        });
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setView(promptView);
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+
+        alertDialogBuilder.create().show();
+
+    }
+
+    private String convertTimeToString(int time) {
+        int amountHours = (int) Math.floor(time / AMOUNT_SEC_IN_HOUR);
+        int amountMins = (int) Math.floor((time - amountHours * AMOUNT_SEC_IN_HOUR) / AMOUNT_SEC_IN_MIN);
+        int amountSec = time - amountHours * AMOUNT_SEC_IN_HOUR - amountMins * AMOUNT_SEC_IN_MIN;
+
+        String timerText = "";
+
+        if (amountHours != 0) {
+            timerText += amountHours + ":";
+        }
+        timerText += String.format("%02d", amountMins) + ":" + String.format("%02d", amountSec);
+
+        return timerText;
     }
 }
+

fd84d5e5b1f12f8856ed306996e71cb2642cea75
Luca Renders <luca.renders@hotmail.com>
2019-03-14 14:18:28 +0100

    Fix code smells

--- app/src/main/java/com/aurora/souschef/MainActivity.java
+++ app/src/main/java/com/aurora/souschef/MainActivity.java
@@ -3,15 +3,17 @@ package com.aurora.souschef;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.aurora.auroralib.Constants;
-import com.aurora.souschef.R;
-import com.aurora.souschefprocessor.facade.Communicator;
-import com.aurora.souschefprocessor.recipe.Recipe;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
+
+    public MainActivity() {
+        // default constructor
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +27,13 @@ public class MainActivity extends AppCompatActivity {
             if (intentThatStartedThisActivity.hasExtra(Constants.PLUGIN_INPUT_TEXT)) {
                 String inputText = intentThatStartedThisActivity.getStringExtra(Constants.PLUGIN_INPUT_TEXT);
                 mTextView.setText(inputText);
-                //Recipe recipe = Communicator.delegate(inputText);
-                //String result = basicPluginObject.getResult();
-                //mTextView.setText(result);
-            }
-            // TODO handle a PluginObject that was cached
-            else if (intentThatStartedThisActivity.hasExtra(Constants.PLUGIN_INPUT_OBJECT)){
+                // Not implemented yet Recipe recipe = Communicator.delegate(inputText);
+                // Not implemented yet String result = basicPluginObject.getResult();
+                // Not implemented yet mTextView.setText(result);
+            } else if (intentThatStartedThisActivity.hasExtra(Constants.PLUGIN_INPUT_OBJECT)) {
+                // TODO handle a PluginObject that was cached
+                //dummy
+                Log.d("NO IMPLEMENTATION", "not implemented yet");
 
             }
         }

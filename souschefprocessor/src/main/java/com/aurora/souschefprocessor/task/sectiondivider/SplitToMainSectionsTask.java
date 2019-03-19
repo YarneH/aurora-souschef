243fac0a40faac8884c31c9c66c8c5364a48c1ee
Luca Renders <luca.renders@hotmail.com>
2019-03-19 12:06:17 +0100

    Start ruleBased implementation

--- souschefprocessor/src/main/java/com/aurora/souschefprocessor/task/sectiondivider/SplitToMainSectionsTask.java
+++ souschefprocessor/src/main/java/com/aurora/souschefprocessor/task/sectiondivider/SplitToMainSectionsTask.java
@@ -3,10 +3,32 @@ package com.aurora.souschefprocessor.task.sectiondivider;
 import com.aurora.souschefprocessor.task.AbstractProcessingTask;
 import com.aurora.souschefprocessor.task.RecipeInProgress;
 
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.AnnotationPipeline;
+import edu.stanford.nlp.pipeline.ParserAnnotator;
+import edu.stanford.nlp.pipeline.TokenizerAnnotator;
+import edu.stanford.nlp.pipeline.WordsToSentencesAnnotator;
+import edu.stanford.nlp.trees.Constituent;
+import edu.stanford.nlp.trees.LabeledScoredConstituentFactory;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
+
 /**
  * A AbstractProcessingTask that divides the original text into usable sections
  */
 public class SplitToMainSectionsTask extends AbstractProcessingTask {
+
+    private static String STEP_STARTER_REGEX = ".*(Preparation|Instruction|Instructions|Method|Steps|Directions|Make it)$";
+    private Annotation annotatedText;
 
     public SplitToMainSectionsTask(RecipeInProgress recipeInProgress) {
         super(recipeInProgress);
@@ -22,12 +44,22 @@ public class SplitToMainSectionsTask extends AbstractProcessingTask {
         // TODO all of this could be in seperate threads
         // TODO add check that an original text is contained
         String text = this.mRecipeInProgress.getOriginalText();
-        String ingredients = findIngredients(text);
-        String steps = findSteps(text);
-        String description = findDescription(text);
+        createAnnotatedText(makeEachNewLineASentence(text));
+
+        String[] ingredientsArray = findIngredients(text);
+        String ingredients = ingredientsArray[0];
+        String[] stepsArray = findSteps(ingredientsArray[1]);
+        String steps= stepsArray[0];
+        String description = findDescription(stepsArray[1]);
         modifyRecipe(this.mRecipeInProgress, ingredients, steps, description);
 
     }
+
+    private String makeEachNewLineASentence(String text) {
+        text = text.replace("\n", ".\n");
+        return text;
+    }
+
 
     /**
      * Modifies the recipe so that the ingredientsString, stepsString, mDescription and amountOfPeople
@@ -52,15 +84,18 @@ public class SplitToMainSectionsTask extends AbstractProcessingTask {
      * @param text the text in which to search for mIngredients
      * @return The string representing the mIngredients
      */
-    public String findIngredients(String text) {
+    public String[] findIngredients(String text) {
         // dummy
         // return "500 gram sauce \n 500 gram spaghetti";
-
-        return "8 thin slices baguette\n" +
+        String [] array = new String[2];
+        array[0] = "8 thin slices baguette\n" +
                 "100g (3 oz) smoked salmon, sliced\n" +
                 "sour cream\n" +
                 "capers\n" +
                 "lemon cheeks, to serve";
+        int startIngredientsIndex = text.indexOf(array[0]);
+        array[1] = text.substring(0, startIngredientsIndex) + text.substring(startIngredientsIndex + array[0].length());
+        return array;
     }
 
     /**
@@ -69,15 +104,78 @@ public class SplitToMainSectionsTask extends AbstractProcessingTask {
      * @param text the text in which to search for mRecipeSteps
      * @return The string representing the mRecipeSteps
      */
-    public String findSteps(String text) {
+    public String[] findSteps(String text) {
         // dummy
         // return "Put 500 gram spaghetti in boiling water for 9 minutes.\n"
         // + "Put the sauce in the Microwave for 3 minutes \n"
         //        + "Put them together.";
 
-        return "Toast baguette slices lightly on one side. Layer each round with smoked salmon, top with a dollup of sour \n" +
-                "cream and sprinkle with a few capers and lots of freshly ground black pepper.";
+        //first try rule based
+        String[] result = findStepsRuleBased(text);
+        if (result == null || ("").equals(result[0])) {
+            result = findStepsNLP(text);
+        }
 
+        String[] array = new String[2];
+        array[0] = "Toast baguette slices lightly on one side. Layer each round with smoked salmon, top with a dollup of sour \n" +
+                "cream and sprinkle with a few capers and lots of freshly ground black pepper.";
+        array[1] = "crostini with smoked salmon & sour cream\n";
+        return array;
+
+    }
+
+    private String[] findStepsRuleBased(String text) {
+        String[] array = new String[2];
+        String[] lines = text.split("\n");
+        boolean found = false;
+        StringBuilder bldSteps = new StringBuilder();
+        StringBuilder bldRest = new StringBuilder();
+        for (String line : lines) {
+            if (!found) {
+                Matcher match = Pattern.compile(STEP_STARTER_REGEX).matcher(line);
+
+                if (match.find()) {
+                    found = true;
+                    System.out.println("FOUND: " + line);
+                    array[1] = bldRest.toString();
+                }
+                else{
+                    bldRest.append(line+"\n");
+                }
+            } else {
+                bldSteps.append(line + "\n");
+            }
+
+        }
+        array[0] = bldSteps.toString();
+        System.out.println(bldSteps.toString());
+        return array;
+    }
+
+    private String[] findStepsNLP(String text) {
+
+        List<CoreMap> sentences = annotatedText.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap sentence : sentences) {
+
+
+            Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+            Set<Constituent> treeConstituents = tree.constituents(new LabeledScoredConstituentFactory());
+
+            for (Constituent c : treeConstituents) {
+                // if the sentece starts with a verb
+                if (c.start() == 0 && c.label().toString().equals("VP")) {
+
+                    CoreLabel token = sentence.get(CoreAnnotations.TokensAnnotation.class).get(0);
+                    System.out.println("Start with verb " + token);
+                }
+
+            }
+
+            //System.out.println(tree.firstChild());
+            //System.out.println(sentence + "///" + tree);
+
+        }
+        return null;
     }
 
     /**
@@ -90,6 +188,24 @@ public class SplitToMainSectionsTask extends AbstractProcessingTask {
         // dummy
         // return "A spaghetti recipe";
         return "crostini with smoked salmon & sour cream";
+    }
+
+    /**
+     * Creates annotation pipeline for
+     *
+     * @return Annotation pipeline
+     */
+    private void createAnnotatedText(String text) {
+        Properties props = new Properties();
+        AnnotationPipeline pipeline = new AnnotationPipeline();
+        pipeline.addAnnotator(new TokenizerAnnotator(false, "en"));
+        pipeline.addAnnotator(new WordsToSentencesAnnotator(false));
+        pipeline.addAnnotator(new ParserAnnotator(false, 100));
+        //pipeline.addAnnotator(new ParserAnnotator());
+        //pipeline.addAnnotator(new MorphaAnnotator(false));
+        annotatedText = new Annotation(text);
+        pipeline.annotate(annotatedText);
+
     }
 
 
--- souschefprocessor/src/test/java/com/aurora/souschefprocessor/task/SplitToMainSectionsTaskTest.java
+++ souschefprocessor/src/test/java/com/aurora/souschefprocessor/task/SplitToMainSectionsTaskTest.java
@@ -9,19 +9,27 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class SplitToMainSectionsTaskTest {
-    private static RecipeInProgress recipe;
-    private static SplitToMainSectionsTask splitToMainSectionsTask;
-    private static String originalText;
+    private static RecipeInProgress recipe1;
+    private static SplitToMainSectionsTask splitToMainSectionsTask1;
+    private static SplitToMainSectionsTask splitToMainSectionsTask2;
+    private static String originalText1;
+    private static String originalText2;
+    private static RecipeInProgress recipe2;
+
 
     @BeforeClass
     public static void initialize() throws IOException, ClassNotFoundException {
-        originalText = initializeRecipeText();
-        recipe = new RecipeInProgress(originalText);
-        splitToMainSectionsTask = new SplitToMainSectionsTask(recipe);
+        String [] recipeTexts = initializeRecipeText();
+        originalText1 = recipeTexts[0];
+        recipe1 = new RecipeInProgress(originalText1);
+        originalText2 = recipeTexts[1];
+        recipe2 = new RecipeInProgress(originalText2);
+        splitToMainSectionsTask1 = new SplitToMainSectionsTask(recipe1);
+        splitToMainSectionsTask2 = new SplitToMainSectionsTask(recipe2);
     }
 
-    private static String initializeRecipeText() {
-        return ("crostini with smoked salmon & sour cream\n" +
+    private static String[] initializeRecipeText() {
+        String [] array = {"crostini with smoked salmon & sour cream\n" +
                 "serves 1\n\n" +
                 "This is one of those effortless starters that feels a little bit special \n" +
                 "but can be made in a flash from ingredients from your supermarket.\n\n" +
@@ -36,32 +44,60 @@ public class SplitToMainSectionsTaskTest {
                 "lemon cheeks, to serve\n\n" +
                 "Toast baguette slices lightly on one side. Layer each round with smoked salmon, top with a dollup of sour \n" +
                 "cream and sprinkle with a few capers and lots of freshly ground black pepper.\n\n\n"
-        );
+                , "Yield\n" +
+                "    4 servings\n" +
+                "Active Time\n" +
+                "    30 minutes\n" +
+                "Total Time\n" +
+                "    35 minutes\n" +
+                "\n" +
+                "Ingredients\n" +
+                "\n" +
+                "        1 lb. linguine or other long pasta\n" +
+                "        Kosher salt\n" +
+                "        1 (14-oz.) can diced tomatoes\n" +
+                "        1/2 cup extra-virgin olive oil, divided\n" +
+                "        1/4 cup capers, drained\n" +
+                "        6 oil-packed anchovy fillets\n" +
+                "        1 Tbsp. tomato paste\n" +
+                "        1/3 cup pitted Kalamata olives, halved\n" +
+                "        2 tsp. dried oregano\n" +
+                "        1/2 tsp. crushed red pepper flakes\n" +
+                "        6 oz. oil-packed tuna\n" +
+                "\n" +
+                "Preparation\n" +
+                "\n" +
+                "        Cook pasta in a large pot of boiling salted water, stirring occasionally, until al dente. Drain pasta, reserving 1 cup pasta cooking liquid; return pasta to pot.\n" +
+                "        While pasta cooks, pour tomatoes into a fine-mesh sieve set over a medium bowl. Shake to release as much juice as possible, then let tomatoes drain in sieve, collecting juices in bowl, until ready to use.\n" +
+                "        Heat 1/4 cup oil in a large deep-sided skillet over medium-high. Add capers and cook, swirling pan occasionally, until they burst and are crisp, about 3 minutes. Using a slotted spoon, transfer capers to a paper towel–lined plate, reserving oil in skillet.\n" +
+                "        Combine anchovies, tomato paste, and drained tomatoes in skillet. Cook over medium-high heat, stirring occasionally, until tomatoes begin to caramelize and anchovies start to break down, about 5 minutes. Add collected tomato juices, olives, oregano, and red pepper flakes and bring to a simmer. Cook, stirring occasionally, until sauce is slightly thickened, about 5 minutes. Add pasta, remaining 1/4 cup oil, and 3/4 cup pasta cooking liquid to pan. Cook over medium heat, stirring and adding remaining 1/4 cup pasta cooking liquid to loosen if needed, until sauce is thickened and emulsified, about 2 minutes. Flake tuna into pasta and toss to combine.\n" +
+                "        Divide pasta among plates. Top with fried capers. "};
+        return array;
     }
 
     @After
     public void wipeRecipe() {
-        recipe.setIngredients(null);
-        recipe.setDescription(null);
-        recipe.setRecipeSteps(null);
+        recipe1.setIngredients(null);
+        recipe1.setDescription(null);
+        recipe1.setRecipeSteps(null);
     }
 
     @Test
     public void SplitToMainSections_doTask_sectionsAreSet() {
-        splitToMainSectionsTask.doTask();
-        assert (recipe.getStepsString() != null);
-        assert (recipe.getIngredientsString() != null);
-        assert (recipe.getDescription() != null);
+        splitToMainSectionsTask1.doTask();
+        assert (recipe1.getStepsString() != null);
+        assert (recipe1.getIngredientsString() != null);
+        assert (recipe1.getDescription() != null);
     }
 
     @Test
     public void SplitToMainSections_doTask_sectionsHaveCorrectValues() {
-        splitToMainSectionsTask.doTask();
-        assert (recipe.getDescription().equals("crostini with smoked salmon & sour cream"));
-        assert (recipe.getStepsString().equals("Toast baguette slices lightly on one side. " +
+        splitToMainSectionsTask1.doTask();
+        assert (recipe1.getDescription().equals("crostini with smoked salmon & sour cream"));
+        assert (recipe1.getStepsString().equals("Toast baguette slices lightly on one side. " +
                 "Layer each round with smoked salmon, top with a dollup of sour \n" +
                 "cream and sprinkle with a few capers and lots of freshly ground black pepper."));
-        assert (recipe.getIngredientsString().equals("8 thin slices baguette\n" +
+        assert (recipe1.getIngredientsString().equals("8 thin slices baguette\n" +
                 "100g (3 oz) smoked salmon, sliced\n" +
                 "sour cream\n" +
                 "capers\n" +

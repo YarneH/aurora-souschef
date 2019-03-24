6630c70d1594eee77077c079c965d13d676bd09a
jotack <jonas.tack@ugent.be>
2019-03-24 14:04:31 +0100

    Add comments and fix Sonar issue

--- app/src/main/java/com/aurora/souschef/Tab3Steps.java
+++ app/src/main/java/com/aurora/souschef/Tab3Steps.java
@@ -52,7 +52,7 @@ public class Tab3Steps extends Fragment {
 
     private StepsPagerAdapter mStepsPagerAdapter;
     private ViewPager mViewPager;
-    private Recipe mRecipe = null;
+    private static Recipe mRecipe = null;
     private static String[] mDescriptionSteps = null;
 
     public Tab3Steps() {
@@ -73,18 +73,25 @@ public class Tab3Steps extends Fragment {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) rootView.findViewById(R.id.vp_steps);
         mViewPager.setAdapter(mStepsPagerAdapter);
+
         // Prevent ViewPager from resetting timers
         mViewPager.setOffscreenPageLimit(mStepsPagerAdapter.getCount());
 
         return rootView;
     }
 
-
+    /**
+     * Classic setter for the Recipe, used to communicate the recipe from the Main Activity
+     */
     protected void setRecipe(Recipe recipe) {
         mRecipe = recipe;
     }
 
-    private void prepareRecipeParts() {
+    /**
+     * Takes the recipe and transforms it into String-representations used for the TextViews
+     * TODO Upgrade this function (timers, step ingredient,...)
+     */
+    private static void prepareRecipeParts() {
         int stepsCount = mRecipe.getRecipeSteps().size();
         mDescriptionSteps = new String[stepsCount];
 
@@ -92,7 +99,6 @@ public class Tab3Steps extends Fragment {
             mDescriptionSteps[i] = mRecipe.getRecipeSteps().get(i).getDescription();
         }
     }
-
 
     /**
      * A placeholder fragment containing the view of a step of the recipe

0db0036e44a53db595e870a98c8be84912bd30fb
jotack <jonas.tack@ugent.be>
2019-03-27 19:06:59 +0100

    Add simple buttons and changing amount of people
    Very basic, for example: no bounds yet

--- app/src/main/java/com/aurora/souschef/Tab2Ingredients.java
+++ app/src/main/java/com/aurora/souschef/Tab2Ingredients.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.aurora.souschefprocessor.recipe.Recipe;
 
@@ -17,7 +19,7 @@ import com.aurora.souschefprocessor.recipe.Recipe;
 public class Tab2Ingredients extends Fragment {
     private Recipe mRecipe = null;
     // The number of people the user picked.
-    private int mActualNumberOfPeople = 0;
+    private int mActualNumberOfPeople = 4;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -29,9 +31,33 @@ public class Tab2Ingredients extends Fragment {
         // Setup recycler view.
         RecyclerView mIngredientList = rootView.findViewById(R.id.rv_ingredient_list);
         mIngredientList.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
         // Feed Adapter
         IngredientAdapter ingredientAdapter = new IngredientAdapter(mRecipe.getIngredients());
         mIngredientList.setAdapter(ingredientAdapter);
+
+        // Prepare parts for amount of people
+        TextView amountTextView = rootView.findViewById(R.id.tv_amount_people);
+        amountTextView.setText(String.valueOf(mActualNumberOfPeople));
+
+        Button addButton = rootView.findViewById(R.id.btn_add);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mActualNumberOfPeople++;
+                amountTextView.setText(String.valueOf(mActualNumberOfPeople));
+            }
+        });
+
+        Button minusButton = rootView.findViewById(R.id.btn_minus);
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mActualNumberOfPeople--;
+                amountTextView.setText(String.valueOf(mActualNumberOfPeople));
+            }
+        });
+
         return rootView;
     }
 

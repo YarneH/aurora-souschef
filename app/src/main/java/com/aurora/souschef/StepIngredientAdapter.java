f0a3808e6323da968935c9485b63499d583c08c6
lrenders <luca.renders@ugent.be>
2019-05-08 11:33:21 +0200

    Fix remaining smells in dev

--- app/src/main/java/com/aurora/souschef/StepIngredientAdapter.java
+++ app/src/main/java/com/aurora/souschef/StepIngredientAdapter.java
@@ -3,7 +3,6 @@ package com.aurora.souschef;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -114,7 +113,7 @@ public class StepIngredientAdapter extends RecyclerView.Adapter<StepIngredientAd
             }
 
             // Set TextView of unit to GONE when it has no unit
-            if ("".equals(ingredient.getUnit())){
+            if ("".equals(ingredient.getUnit())) {
                 mIngredientUnit.setVisibility(View.GONE);
             } else {
                 mIngredientUnit.setText(ingredient.getUnit());

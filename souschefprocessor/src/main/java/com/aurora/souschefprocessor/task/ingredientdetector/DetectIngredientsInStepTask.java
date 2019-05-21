4560df6ff104a0fbbcb6851c2a9e95e9d74ab643
Piet Verheye <piet.verheye@ugent.be>
2019-05-21 16:16:55 +0200

    Documentation of constants in DetectIngredientInStepTask

--- souschefprocessor/src/main/java/com/aurora/souschefprocessor/task/ingredientdetector/DetectIngredientsInStepTask.java
+++ souschefprocessor/src/main/java/com/aurora/souschefprocessor/task/ingredientdetector/DetectIngredientsInStepTask.java
@@ -55,17 +55,20 @@ public class DetectIngredientsInStepTask extends DetectIngredientsTask {
     private static final String OF_PREPOSITION = "of";
 
     /**
-     * TODO: @Piet piet verheyhe
+     * A constant for the length of a preposition in front of an ingredient in the step,
+     * it is used for setting a search bound for finding the properties of the ingredient
      */
     private static final int PREPOSITION_LENGTH = 1;
 
     /**
-     * TODO: @Piet piet verheye
+     * A constant for the length of a fraction in front of an ingredient in the step,
+     * it is used for setting a search bound for finding the properties of the ingredient
      */
     private static final int FRACTIONS_LENGTH = 1;
 
     /**
-     * TODO: @Piet piet verhey
+     * A constant for the maximum length of a quantity in front of an ingredient in the step,
+     * it is used for setting a search bound for finding the properties of the ingredient
      */
     private static final int MAX_QUANTITY_LENGTH = 2;
 
